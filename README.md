a30e20b257be51077e84bd29e524cee9689c2311
laobubu <laobubu@gmail.com>
2016-10-05 22:30:20 +0800

    clip

---
+++ README.md
@@ -0,0 +1,12 @@
+QRScaner
+========
+
+A QR Code scaner extension for Firefox.
+
+Using [jsQRCode](https://github.com/edi9999/jsqrcode) library to implement offline decoding.
+
+See
+
+- <http://laobubu.net/QRScaner>
+- <https://addons.mozilla.org/firefox/addon/qrcode-scanneroffline/>
+- <http://blog.laobubu.net/643>
--- chrome/content/popup.js
+++ chrome/content/popup.js
@@ -25,6 +25,13 @@ qr.callback = function (result, err) {
     }
 }
 
+var clickToOpen = $('#clickToOpen');
+clickToOpen.addEventListener('click', function () {
+    var url = $('#result').value;
+    if (!/^[a-z]+:/.test(url)) url = 'http://' + url;
+    window.open(url, '_blank');
+}, false);
+
 function setResult(result, success) {
     var info = $('#info')
 
@@ -32,6 +39,15 @@ function setResult(result, success) {
     else info.classList.add('is-failed');
 
     $('#result').value = result;
+
+    if (success) {
+        $('#clip').style.display = 'none';
+        if (/^([a-z-]+:.+|\w[\.\w]+\/.+)$/im.test(result)) {
+            clickToOpen.style.display = '';
+        } else {
+            clickToOpen.style.display = 'none';
+        }
+    }
 }
 
 function decodeOffline() {
@@ -80,13 +96,115 @@ function decodeOnline() {
 decodeOffline();
 $('#decodeOnline').addEventListener('click', decodeOnline, false);
 
+// clip
+
+var clipImage = (function () {
+
+    var clip = $('#clip');
+    var zoom = 1;
+    var canvas = $('#clip-canvas');
+    var ctx = canvas.getContext("2d");
+
+    var x1 = -1, y1 = -1, x2, y2;
+    var x, y, w, h;
+
+    function calcXYWH() {
+        w = x2 - x1, h = y2 - y1;
+        if (w < 0) { x = x2; w = -w } else { x = x1 }
+        if (h < 0) { y = y2; h = -h } else { y = y1 }
+    }
+
+    canvas.addEventListener('mousedown', function (ev) {
+        if (!(ev.buttons & 1)) return;
+
+        x1 = ev.offsetX;
+        y1 = ev.offsetY;
+    }, false)
+
+    canvas.addEventListener('mousemove', function (ev) {
+        if (!(ev.buttons & 1)) return;
+
+        x2 = ev.offsetX;
+        y2 = ev.offsetY;
+        calcXYWH();
+
+        ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+        if (prefs.getBoolPref('otsu')) imgproc.otsu(ctx, x, y, w, h);
+
+        ctx.beginPath();
+        ctx.lineWidth = "2";
+        ctx.strokeStyle = "green";
+        ctx.rect(x, y, w, h);
+        ctx.stroke();
+    }, false)
+
+    canvas.addEventListener('mouseup', function (ev) {
+        if (ev.button != 0) return;
+
+        x2 = ev.offsetX;
+        y2 = ev.offsetY;
+        calcXYWH();
+
+        window.canvas.width = w;
+        window.canvas.height = h;
+        window.ctx.drawImage(canvas, -x, -y);
+
+        decodeOffline();
+        ctx.font = "12pt";
+        ctx.fillStyle = "#0F0";
+        ctx.fillText("Please retry", x, y);
+    }, false)
+
+    clip.addEventListener('wheel', function (ev) {
+        var d = ev.deltaY;
+        var v = zoom;
+
+        if (d < 0) v += .1;
+        if (d > 0) v -= .1;
+
+        if (v < .2) v = .2;
+        if (v > 2.) v = 2.;
+        zoom = v;
+
+        afterZoomUpdated();
+
+        ev.preventDefault();
+        ev.stopPropagation();
+    }, true)
+
+    function afterZoomUpdated() {
+        canvas.width = zoom * img.width;
+        canvas.height = zoom * img.height;
+        ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+    }
+
+    function startClip() {
+        zoom = 1;
+        afterZoomUpdated();
+        clip.style.display = '';
+    }
+
+    return startClip;
+})();
+
 // auto close
 
 setTimeout(function () {
-    var chk = $('#autoClose');
-    chk.addEventListener('click', function () {
-        prefs.setBoolPref('autoclose', chk.checked)
-    }, false);
-    chk.checked = prefs.getBoolPref('autoclose')
     window.onblur = function () { prefs.getBoolPref('autoclose') && window.close() };
 }, 50);
+
+// prefs checkboxs
+
+function prefsUpdate(ev) {
+    prefs.setBoolPref(ev.target.getAttribute('data-prefs'), ev.target.checked)
+}
+
+[].forEach.call($$('[data-prefs]'), function (cbox) {
+    var name = cbox.getAttribute('data-prefs');
+    try {
+        cbox.checked = prefs.getBoolPref(name);
+    } catch (e) {
+        prefs.setBoolPref(name, cbox.checked);
+    }
+    cbox.addEventListener('change', prefsUpdate, false);
+})
